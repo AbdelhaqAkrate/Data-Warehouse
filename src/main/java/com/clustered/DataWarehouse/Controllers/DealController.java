bf601db21f8f863e48633af68990e633b385c55d
Abdelhaq Akrate <abdelhaq.akrate20@gmail.com>
2025-07-18 21:33:53 +0100

    feature5 : integrate swagger documentation

--- src/main/java/com/clustered/DataWarehouse/Controllers/DealController.java
+++ src/main/java/com/clustered/DataWarehouse/Controllers/DealController.java
@@ -4,6 +4,8 @@ import com.clustered.DataWarehouse.DTO.DealDTO;
 import com.clustered.DataWarehouse.DTO.DealListDTO;
 import com.clustered.DataWarehouse.Responses.ApiResponse;
 import com.clustered.DataWarehouse.Services.DealService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +15,12 @@ import java.util.ArrayList;
 @RestController
 @RequestMapping("/api/deals")
 @RequiredArgsConstructor
+@Tag(name = "Deals", description = "Creating a deal or many deals")
 public class DealController {
 
     private final DealService dealService;
 
+    @Operation(summary = "Create a single deal")
     @PostMapping("/create")
     public ResponseEntity<ApiResponse> create(@RequestBody DealDTO dealDTO) {
         DealDTO created = dealService.create(dealDTO);
@@ -24,6 +28,7 @@ public class DealController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Operation(summary = "Insert many deals")
     @PostMapping("/insert")
     public ResponseEntity<ApiResponse> insert(@RequestBody DealListDTO dealListDTO) {
         var result = dealService.insert(dealListDTO.getDeals());
--- src/main/java/com/clustered/DataWarehouse/Repositories/DealRepository.java
+++ src/main/java/com/clustered/DataWarehouse/Repositories/DealRepository.java
@@ -2,5 +2,7 @@ package com.clustered.DataWarehouse.Repositories;
 
 import com.clustered.DataWarehouse.Models.Deal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DealRepository extends JpaRepository<Deal, String> { }
